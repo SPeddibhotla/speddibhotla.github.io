22f4b2add00f86f12adbd18a8d7e0cef62ce342c
SPeddibhotla <sudhakar.peddibhotla@gmail.com>
2019-05-27 10:39:00 -0700

    Sample React Page

---
+++ precache-manifest.e33ae96188ec0370f5da3f03a8e6df95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d9cff2314706e86f02da250223638a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a48498c1f9102b39337",
+    "url": "/static/css/main.a713b3b4.chunk.css"
+  },
+  {
+    "revision": "f5f788f3713acc6acd9b",
+    "url": "/static/js/2.77932c2c.chunk.js"
+  },
+  {
+    "revision": "9a48498c1f9102b39337",
+    "url": "/static/js/main.e84207ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
